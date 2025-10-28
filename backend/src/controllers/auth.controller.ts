3ced0a128659267ecf6430310a5154f4c6201939
SantiagoComas07 <santiagocomas07d@gmail.com>
2025-10-28 06:14:52 -0500

    feat:Register logic in controller file

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,7 +3,7 @@ import {User} from '../models';
 import passwordService from '../services/encrypted';
 import jwt from 'jsonwebtoken';
 import {JWT_SECRET}from '../index'
-
+import bcrypt from 'bcryptjs';
 
 // log in 
 
@@ -53,4 +53,43 @@ export  const login = async(req:Request, res:Response ): Promise<void>=>{
 }catch(error){
     console.error('Error during login:', error);
 }
+}
+
+
+
+
+// Register new User
+
+export const register = async(req:Request, res:Response):Promise<void>=>{
+    try{
+        const {name, email, password, role} = req.body;
+         // Validate that the fields are present
+        if(!email || !password){
+            res.status(400).json({
+                success:false,
+                message:'Email and password are required'
+            })
+            return;
+        };
+        const existUser = await User.findOne({where:{email}});
+        if(existUser){
+            res.status(400).json({message:'The user already exist'});
+            return;
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+            role: role || 'analyst',
+        });
+        await newUser.save();
+
+            res.status(201).json({
+      message: "Usuario registrado exitosamente",});
+    }catch(error){
+        console.error('Error during registration:', error)
+    }
 }
